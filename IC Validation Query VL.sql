3760ace78e747eb57e4a4376b5f01587f92b5a82
Jay-louis <51897603+Jay-louis@users.noreply.github.com>
2020-09-23 19:10:42 -0700

    Update IC Validation Query VL.sql

--- IC Validation Query VL.sql
+++ IC Validation Query VL.sql
@@ -19,7 +19,7 @@ select order_number, item_number, order_qty, picked_qty, order_qty - picked_qty 
   WHERE type = '0' 
   AND location_id NOT LIKE 'LOST%'
   AND( location_id LIKE 'BS%'
-  OR location_id LIKE 'A[A-B]&'
+  OR location_id LIKE 'A[A-D]%'
   OR location_id LIKE '%STAGE%'
   OR location_id LIKE '%OS%'
   OR location_id LIKE '%RB%'
@@ -42,7 +42,7 @@ select order_number, item_number, order_qty, picked_qty, order_qty - picked_qty 
   WHERE type IN ('-1', '-85', '-80') 
   AND location_id NOT LIKE 'LOST%'
   AND( location_id LIKE 'BS%'
-  OR location_id LIKE 'A[A-B]&'
+  OR location_id LIKE 'A[A-D]%'
   OR location_id LIKE '%STAGE%'
   OR location_id LIKE '%OS%'
   OR location_id LIKE '%RB%'
@@ -62,7 +62,7 @@ select order_number, item_number, order_qty, picked_qty, order_qty - picked_qty 
 from t_order o (nolock)
 join t_order_detail d (nolock) on d.order_number = o.order_number
 join t_pick_detail p (nolock) on p.order_number = o.order_number and p.line_number = d.line_number and p.item_number = d.item_number and p.status not in ('CANCELLED')
-where cast(order_date as date) = '2020-09-16' and o.status NOT IN ('SHIPPED','CANCELLED','PACKED','LOADED','S', 'PROCESSING', 'SHIPPING')
+where cast(order_date as date) = '2020-09-17' and o.status NOT IN ('SHIPPED','CANCELLED','PACKED','LOADED','S', 'PROCESSING', 'SHIPPING')
 and o.order_type IN ('SM', 'EO', 'ECOM')
 and wcs_status in ('R', 'M', 'P', 'S', 'C', 'A')
 group by o.order_number, d.item_number) a
