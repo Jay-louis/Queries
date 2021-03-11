bc6577619091628359d0a0daf0c5850562ff1b43
Jay-louis <51897603+Jay-louis@users.noreply.github.com>
2021-03-10 19:52:12 -0800

    Update 0 PM QTY.sql

--- 0 PM QTY.sql
+++ 0 PM QTY.sql
@@ -1,14 +1,11 @@
 DECLARE @bld1 as NVARCHAR(25)
 SET @bld1 = 'BS-%'
 DECLARE @bld2 as NVARCHAR(25)
-SET @bld2 = 'BS-%'
+SET @bld2 = 'RB-%'
  
  select 
- e.item_number, e.pm_qty, e.bs_qty, e.scrb_qty, e.stg_qty, e.total_qty, e.location_id, e.hu_id,
- e.item_number as item_number2, e.actual_qty, 
- (case when m.size = 'OS' then 'OS' else 'NOT OS' end) SFS,/* m.style, m.color,*/ m.size,
- (CASE WHEN m.size LIKE '%' THEN 'Urgent' END) "replen_status",
- 
+ e.*, 
+ (case when m.size = 'OS' then 'OS' else 'NOT OS' end) SFS, m.style, m.color, m.size, 
   (case when location_id like 'RB-%' then 'RIM'
   when location_id like 'SV-%' or location_id like 'SC-%' then 'SEVILLE'
   when location_id like 'BS-%' then 'SFS' end) building from (
@@ -22,7 +19,7 @@ i.item_number,
 isnull((
 		  SELECT sum(isnull(pm.actual_qty,0)) AS pm_qty
 		  FROM t_stored_item pm (nolock)
-		  WHERE type = '0'  
+		  WHERE type = '0' AND status = 'A'
 		  AND (location_id LIKE 'AA%' OR location_id LIKE 'AB%' OR location_id LIKE 'AC%' OR location_id LIKE 'AD%') 
 		  AND pm.item_number = i.item_number
 		  GROUP BY item_number
@@ -64,8 +61,10 @@ isnull((
 		 ),0) stg_qty
 from t_stored_item i (nolock)
 group by i.item_number) a
-where (scrb_qty > 0 or bs_qty > 0) and pm_qty <= 5) b -- change as needed for pm qty
+where (scrb_qty > 0 or bs_qty > 0) and pm_qty <= 0) b
 join t_stored_item i (nolock) on i.item_number = b.item_number and i.type = '0' 
-AND (i.location_id LIKE @bld1 or i.location_id like @bld2)) e -- Bulk locations (SFS only or include RB/SEV)
+AND (i.location_id LIKE @bld1 
+	or i.location_id like 'SC-%' or i.location_id like 'SV-%' or i.location_id like @bld2
+)) e -- Bulk locations (SFS only or include RB/SEV)
 join t_item_master m (nolock) on m.item_number = e.item_number
-where rowNumber = 1 and stg_qty = 0
+where (rowNumber = 1 and stg_qty = 0)
